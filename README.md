2fad60c9b4aa5916726b25745db1a757a7d0596f
Mayukh Mondal <21051405@kiit.ac.in>
2023-06-17 06:07:30 +0530

    testing-turbo

--- README.md
+++ README.md
@@ -1,78 +1,41 @@
-<h1>Solving for India (Hackathon Series)</h1>
-<h2>Project Theme : Fintech</h2>
-
-[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://deepraj21-realtime-stock-predictor-app-fhpeiz.streamlit.app/)
-
-----
-
 <h1 align="center">Predictbay</h1>
-<img align="right" src="https://cdni.iconscout.com/illustration/premium/thumb/stock-market-investment-4268268-3569430.png">
+<h5 align="center">"Master the Art of Stock Forecasting: Harness the Power of Machine Learning"</h5>
 
-### Problem Statement :
-The stock market is volatile and unpredictable, making it difficult for investors to make informed decisions about when to buy or sell their investments. There is a need for a real-time stock market predictor that can help investors make better decisions by predicting future stock prices accurately.
+<hr>
 
-### Solution Description :
-We propose to build a real-time stock market predictor using machine learning. The system will collect real-time and historical stock market data from different industries and preprocess the data to remove outliers and missing values. We will then use feature engineering techniques to select the most relevant features that can affect the stock market's performance.
+<img align="right" height="300px" src="https://cdni.iconscout.com/illustration/premium/thumb/man-trading-stocks-7113747-5783456.png?f=webp">
 
-We will train a Deep learning model such as LSTM on the preprocessed data to predict future stock prices accurately. The model will be loaded in the Streamlit app using different modules/Libraries to generate real-time predictions. The app will display interactive charts and graphs that can show the predicted stock prices in real-time.
+# Project Overview
 
-To ensure that the model is accurate and reliable, we will use cross-validation techniques and evaluate the model's performance using metrics such as Mean Squared Error (MSE), Root Mean Squared Error (RMSE), and Mean Absolute Error (MAE). We will also test the model on a separate test set to ensure that it can generalize well to new data.
+The project aims to provide stock price predictions for a given ticker symbol using historical stock data. The web application allows users to input a ticker symbol, and it retrieves the corresponding stock data using the Yahoo Finance API. The application then utilizes two models to predict future stock prices: a traditional machine learning model and a bi-directional LSTM model. The predicted prices are displayed on a web page along with historical price charts and other stock information.
 
-Overall, our real-time stock market predictor will provide investors with a reliable tool that can help them make better-informed decisions about when to buy or sell their investments. By predicting future stock prices accurately, the system can help investors avoid losses and maximize their profits.
+# Web Application
 
-----
+The Flask web application provides a user interface for interacting with the stock prediction models. It uses HTML templates to render the web pages and accepts user inputs for the ticker symbol. It retrieves stock data using the Yahoo Finance API and displays the historical price chart, including moving averages. It also displays the predicted prices from both models, along with other stock information such as the today's high, close, and open values. The application provides additional pages for FAQs, contact information, about section, user registration, and news. It also includes a global chat feature and a login page. Overall, the project combines traditional machine learning techniques with deep learning (LSTM) to predict stock prices. The Flask web application serves as a user-friendly interface to interact with the models and view the predictions, historical data, and other stock-related information.
 
-<h3>A list of the required modules used in the code and the command to install them using pip:</h3>
+<img align="left" height="290px" src="https://cdni.iconscout.com/illustration/premium/thumb/stock-market-and-trading-education-7113777-5783443.png">
 
-```
-pip install numpy
-pip install pandas
-pip install yfinance
-pip install streamlit
-pip install pandas-datareader
-pip install scikit-learn
-pip install tensorflow
-pip install plotly
-pip install requests
-pip install streamlit-lottie
-pip install datetime
-```
+# Machine Learning Model
 
-<img src="templates/image1.png">
-<img src="templates/image2.png">
-<img src="templates/image3.png">
-<img src="templates/image4.png">
+The traditional machine learning model used in the project is a regression model trained on historical stock price data. The model is loaded and the data is preprocessesd by scaling it using MinMaxScaler and splitting it into training and testing sets. The model is trained on the training set and used to predict prices on the testing set. The predicted prices are then inverse-transformed using the scaler to obtain the actual predicted values.
 
-<h2>Future Prediction :</h2>
+# Bi-directional LSTM Model
 
-```
-from keras.models import load_model
-model = load_model('keras_model.h5')
-last_100_days = data_testing[-100:].values
-last_100_days_scaled = scaler.transform(last_100_days)
-predicted_prices = []
-for i in range(1):
-    X_test = np.array([last_100_days_scaled])
-    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    predicted_price = model.predict(X_test)
-    predicted_prices.append(predicted_price)
-    last_100_days_scaled = np.append(last_100_days_scaled, predicted_price)
-    last_100_days_scaled = np.delete(last_100_days_scaled, 0)
-predicted_prices = np.array(predicted_prices)
-predicted_prices = predicted_prices.reshape(predicted_prices.shape[0],predicted_prices.shape[2])
-predicted_prices = scaler.inverse_transform(predicted_prices)
-print('Predicted price for the next day:', predicted_prices[0][0])
-```
-<p><i>This code block is loading a trained Keras model, using it to make predictions on the next day's stock price based on the last 100 days of historical data, and then printing out the predicted price</i>
+The bi-directional LSTM model is implemented using the Keras library.The Data_fetch_transform function is responsible for fetching the stock data, preprocessing it, and splitting it into training and testing sets. The model is trained on the training set using the LSTM architecture with a time step of 100. The training and testing sets are reshaped to fit the model's input shape. The model is used to predict prices on the testing set, which are then inverse-transformed using the scaler. Additionally, the code includes an evaluate_predictions function that calculates the difference and ratio between the predicted and actual prices and identifies outliers based on a given threshold. The model also predicts the next day's closing price.
 
-The `load_model` function from Keras is used to load the trained model from a saved file named `keras_model.h5`.
+<hr>
 
-The next step is to get the last 100 days of historical data from the testing dataset and scale the data using the `scaler.transform` function. The scaler was previously fit to the training dataset and is used to normalize the testing data.
+# Predictbay Features
 
-An empty list called `predicted_prices` is created to store the predicted prices. The for loop iterates only once and creates an array `X_test` containing the last 100 days of scaled data. The array `X_test` is then reshaped to match the input shape of the trained model, which is `(number of samples, number of timesteps, number of features)`. In this case, there is only one feature (the stock price), so the input shape is `(1, 100, 1)`.
+<img src="Predictbay_Template/img1.png">
+<img src="Predictbay_Template/img2.png">
+<img src="Predictbay_Template/img3.png">
+<img src="Predictbay_Template/img4.png">
+<img src="Predictbay_Template/img5.png">
+<img src="Predictbay_Template/img6.png">
 
-The `model.predict` function is used to generate a prediction for the next day's stock price based on the 100 days of historical data. The predicted price is then appended to the `predicted_prices` list, and the oldest data point in `last_100_days_scaled` is replaced with the predicted price. This updated list is then used in the next iteration of the loop to generate a prediction for the following day.
 
-Once the loop is finished, the predicted prices are inverted back to their original scale using the `scaler.inverse_transform` function. The predicted price for the next day is then printed to the console using `print('Predicted price for the next day:', predicted_prices[0][0])`.
 
-</p>
+
+
+
--- app.py
+++ app.py
@@ -1,101 +1,436 @@
-import streamlit.components.v1 as components
-from keras.models import load_model
+from flask import Flask, render_template, request, redirect
+import yfinance as yf
 import numpy as np
 import pandas as pd
-import yfinance as yf
-import streamlit as st
-import pandas_datareader as data
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import load_model
-import plotly.graph_objs as go
-import plotly.express as px
-import requests
+from pandas import DatetimeIndex
+import json
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import sklearn.metrics as metrics
+import math
+from time import sleep
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from matplotlib.pylab import rcParams
+import matplotlib.pyplot as plt
 from datetime import datetime
-from streamlit_lottie import st_lottie
-from yfinance import shared
+import warnings
+from sklearn.model_selection import train_test_split as split
+from sklearn.preprocessing import MinMaxScaler
+import warnings
+import itertools
 
-start = '2010-01-01'
-end = datetime.now()
+warnings.filterwarnings("ignore")
+from IPython import display
+from matplotlib import pyplot
+import os
+import re
+import seaborn as sns
+import plotly.express as px
+import warnings
+from matplotlib.patches import Patch
+import yfinance as yf
+import tensorflow as tf
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import json
+from math import floor
+import threading
+from queue import Queue
+from turbo_flask import Turbo
 
-ticker = st.text_input('Enter a Valid stock Ticker', 'AAPL')
+app = Flask(__name__)
 
-df = yf.download(ticker, start=start, end=end)
+turbo = Turbo(app)
 
-data_training = pd.DataFrame(df['Close'][0:int(len(df)*70)])
-data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
 
-scaler = MinMaxScaler(feature_range=(0, 1))
+class InvalidTickerError(Exception):
+    pass
 
-data_training_array = scaler.fit_transform(data_training)
 
-x_train = []
-y_train = []
+def get_data(ticker, period):
+    try:
+        df = yf.download(ticker, period=period)
+        if df.empty:
+            raise InvalidTickerError(f"Invalid ticker: {ticker}")
+        return df
+    except Exception as e:
+        raise InvalidTickerError(f"Invalid ticker: {ticker}") from e
 
-for i in range(100, data_training_array.shape[0]):
-    x_train.append(data_training_array[i-100: i])
-    y_train.append(data_training_array[i, 0])
 
-x_train, y_train = np.array(x_train), np.array(y_train)
+def Data_fetch_transform(data):
+    # data = yf.download(ticker)
+    data['Date'] = pd.to_datetime(data.index, infer_datetime_format=True)
+    data_feature_selected = data.drop(axis=1, labels=["Open", "High", "Low", "Volume"])
+    data_feature_selected['differenced_trasnformation_demand'] = data_feature_selected['Adj Close'].diff().values
+    data_feature_selected['differenced_demand_filled'] = np.where(pd.isnull(data_feature_selected['differenced_trasnformation_demand']), data_feature_selected['Adj Close'], data_feature_selected['differenced_trasnformation_demand'])
+    data_feature_selected['differenced_inv_transformation_demand'] = data_feature_selected['differenced_demand_filled'].cumsum()
+    np.testing.assert_array_equal(data_feature_selected['Adj Close'].values, data_feature_selected['differenced_inv_transformation_demand'].values)
+    current_datetime = datetime.now()
+    # Extract the date portion
+    current_date = current_datetime.date()
+    # Convert the date to a string
+    current_date_string = current_date.strftime('%Y-%m-%d')
+    df1 = data_feature_selected.copy()
+    # mask = (df1['Date'] > '2010-01-01') & (df1['Date'] <= current_date_string)
+    y = df1['Adj Close']
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    y = scaler.fit_transform(np.array(y).reshape(-1, 1))
+    ##splitting dataset into train and test split
+    training_size = int(len(y) * 0.65)
+    test_size = len(y) - training_size
+    train_data, test_data = y[0:training_size, :], y[training_size:len(y), :1]
 
-# load model
-model = load_model('keras_model.h5')
+    def create_dataset(dataset, time_step=1):
+        dataX, dataY = [], []
+        for i in range(len(dataset) - time_step - 1):
+            a = dataset[i:(i + time_step), 0]
+            dataX.append(a)
+            dataY.append(dataset[i + time_step, 0])
+        return np.array(dataX), np.array(dataY)
 
-past_100_days = data_training.tail(100)
+    time_step = 100
+    X_train, y_train = create_dataset(train_data, time_step)
+    X_test, ytest = create_dataset(test_data, time_step)
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+    return X_train, X_test, y_train, ytest, scaler
 
-final_df = past_100_days.append(data_testing, ignore_index=True)
-input_data = scaler.fit_transform(final_df)
 
-x_test = []
-y_test = []
+def biLSTM(ticker, result_queue):
+    bilstm_model = load_model("bilstm_1000_epochs.h5")
+    X_train, X_test, y_train, ytest, scaler = Data_fetch_transform(ticker)
+    train_predict = bilstm_model.predict(X_train)
+    test_predict = bilstm_model.predict(X_test)
+    train_predict = scaler.inverse_transform(train_predict)
+    test_predict = scaler.inverse_transform(test_predict)
+    predictions = bilstm_model.predict(X_test)
 
-for i in range(100, input_data.shape[0]):
-    x_test.append(input_data[i-100:i])
-    y_test.append(input_data[i, 0])
+    def evaluate_predictions(predictions, ytest, outliers):
+        ratio = []
+        differences = []
+        for pred in range(len(ytest)):
+            ratio.append((ytest[pred] / predictions[pred]) - 1)
+            differences.append(abs(ytest[pred] - predictions[pred]))
 
-x_test, y_test = np.array(x_test), np.array(y_test)
+        n_outliers = int(len(differences) * outliers)
+        outliers = pd.Series(differences).astype(float).nlargest(n_outliers)
 
-y_predict = model.predict(x_test)
+        return ratio, differences, outliers
 
-scaler = scaler.scale_
+    ratio, differences, outliers = evaluate_predictions(predictions, ytest, 0.01)
+    for index in outliers.index:
+        outliers[index] = predictions[index]
 
-scale_factor = 1/scaler[0]
-y_predict = y_predict * scale_factor
-y_test = y_test * scale_factor
+    def predict_next_day_closing_price(model, X_test, scaler):
 
-st.subheader('Original VS predicted')
-fig2 = go.Figure()
-fig2.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=y_test, name='Original Price'))
-fig2.add_trace(go.Scatter(x=df.index[int(len(df)*0.70):], y=y_predict[:, 0], name='Predict'))
-fig2.update_layout(
-                    xaxis_title='Date',
-                    yaxis_title='Price',
-                    width=1000,
-                    height=600)
+        X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+        predictions = model.predict(X_test)
+        predictions = scaler.inverse_transform(predictions)
+        return predictions[-1][0]
 
-st.plotly_chart(fig2, use_container_width=True)
+    next_day = predict_next_day_closing_price(bilstm_model, X_test, scaler)
 
-last_100_days = data_testing[-100:].values
+    # return next_day, predictions, ytest
+    result_queue.put((next_day, predictions, ytest))
 
-# Instantiate a scaler object and fit_transform the data
-scaler = MinMaxScaler()
-last_100_days_scaled = scaler.fit_transform(last_100_days)
+def return_index_processed(ticker):
+    try:
+        period = '10y'
+        df = get_data(ticker, period)
 
-# Create an empty list to store the predicted prices
-predicted_prices = []
+        closing_prices = df['Close']
 
-# Make predictions for the next day using the last 100 days of data
-for i in range(1):
-    X_test = np.array([last_100_days_scaled])
-    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    predicted_price = model.predict(X_test)
-    predicted_prices.append(predicted_price)
-    last_100_days_scaled = np.append(last_100_days_scaled, predicted_price)
-    last_100_days_scaled = np.delete(last_100_days_scaled, 0)
+        high_value = get_today_high(ticker)
+        increase_status_high, percentage_change_high = get_percentage_change_high(ticker)
+        close_value = 0
+        increase_status_Close, percentage_change_Close = 0, 0
+        open_value = get_today_open(ticker)
+        increase_status_Open, percentage_change_Open = get_percentage_change_Open(ticker)
 
-# Invert the scaling of the predicted price
-predicted_prices = np.array(predicted_prices)
-predicted_prices = predicted_prices.reshape(predicted_prices.shape[0], predicted_prices.shape[2])
-predicted_prices = scaler.inverse_transform(predicted_prices)
+        chart_data = [{'x': str(date), 'y': price} for date, price in closing_prices.items()]
+        ma100 = closing_prices.rolling(window=100).mean()
+        ma100 = [{'x': str(date), 'y': price} for date, price in ma100.items() if not pd.isna(price)]
+        ma200 = closing_prices.rolling(window=200).mean()
+        ma200 = [{'x': str(date), 'y': price} for date, price in ma200.items() if not pd.isna(price)]
 
-st.header('- Prediction')
-st.write('Predicted price for the next day:', predicted_prices[0][0])
+        data_training = pd.DataFrame(df['Close'][0:int(len(df) * 0.7)])
+        data_testing = pd.DataFrame(df['Close'][int(len(df) * 0.7):int(len(df))])
+
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        data_training_array = scaler.fit_transform(data_training)
+
+        x_train = []
+        y_train = []
+
+        for i in range(100, data_training_array.shape[0]):
+            x_train.append(data_training_array[i - 100: i])
+            y_train.append(data_training_array[i, 0])
+
+        x_train, y_train = np.array(x_train), np.array(y_train)
+
+        # Load model
+        model = load_model('lstm_1000_epochs.h5')
+
+        result_queue = Queue()
+
+        # Creating thread for model prediction
+        bilstm_thread = threading.Thread(target=biLSTM, args=(df, result_queue))
+        bilstm_thread.start()
+
+        past_100_days = data_training.tail(100)
+        final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+
+        input_data = scaler.fit_transform(final_df)
+
+        x_test = []
+        y_test = []
+
+        for i in range(100, input_data.shape[0]):
+            x_test.append(input_data[i - 100:i])
+            y_test.append(input_data[i, 0])
+
+        x_test, y_test = np.array(x_test), np.array(y_test)
+
+        y_predict = model.predict(x_test)
+
+        scaler = scaler.scale_
+
+        scale_factor = 1 / scaler[0]
+        y_predict = y_predict * scale_factor
+        y_test = y_test * scale_factor
+
+        fig2 = go.Figure()
+        fig2.add_trace(go.Scatter(x=df.index[int(len(df) * 0.70):], y=y_test, name='Original Price'))
+        fig2.add_trace(go.Scatter(x=df.index[int(len(df) * 0.70):], y=y_predict[:, 0], name='Predict'))
+        fig2.update_layout(
+            xaxis_title='Date',
+            yaxis_title="Price(standardized)",
+            height=500,
+            plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
+            xaxis=dict(showticklabels=False)
+        )
+        graph_html = fig2.to_html(full_html=False)
+
+        last_100_days = data_testing[-100:].values
+        scaler = MinMaxScaler()
+        last_100_days_scaled = scaler.fit_transform(last_100_days)
+
+        predicted_prices = []
+
+        # Wait for the thread to finish
+        bilstm_thread.join()
+
+        biLSTM_predicted_price, predictions_biLSTM, biLSTM_ytest = result_queue.get()
+        fig3 = go.Figure()
+        fig3.add_trace(go.Scatter(x=df.index[int(len(df) * 0.70):], y=biLSTM_ytest, name='Original Price'))
+        fig3.add_trace(go.Scatter(x=df.index[int(len(df) * 0.70):], y=predictions_biLSTM[:, 0], name='Predict'))
+        fig3.update_layout(
+            xaxis_title='Date',
+            yaxis_title="Price(standardized)",
+            height=500,
+            plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
+            xaxis=dict(showticklabels=False)
+        )
+        bilstm_graph_html = fig3.to_html(full_html=False)
+
+        for i in range(1):
+            X_test = np.array([last_100_days_scaled])
+            X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+            predicted_price = model.predict(X_test)
+            predicted_prices.append(predicted_price)
+            last_100_days_scaled = np.append(last_100_days_scaled, predicted_price)
+            last_100_days_scaled = np.delete(last_100_days_scaled, 0)
+
+        predicted_prices = np.array(predicted_prices)
+        predicted_prices = predicted_prices.reshape(predicted_prices.shape[0], predicted_prices.shape[2])
+        predicted_prices = scaler.inverse_transform(predicted_prices)
+        predicted_price = predicted_prices[0][0]
+
+        if (biLSTM_predicted_price > predicted_price):
+            uprange = floor(biLSTM_predicted_price)
+            downrange = floor(predicted_price)
+        else:
+            uprange = floor(predicted_price)
+            downrange = floor(biLSTM_predicted_price)
+        print(high_value, open_value)
+        return render_template('index.html', high_value=high_value, close_value=close_value, open_value=open_value, ticker=ticker, chart_data=chart_data, predicted_price=round(predicted_price, 2), biLSTM_predicted_price=round(biLSTM_predicted_price, 2), uprange=uprange, downrange=downrange, bilstm_graph_html=bilstm_graph_html, ma100=ma100, ma200=ma200, graph_html=graph_html, high_status=increase_status_high, high_percent=percentage_change_high, Close_status=increase_status_Close, Close_percent=percentage_change_Close, Open_status=increase_status_Open, Open_percent=percentage_change_Open)
+        close_value = get_today_close(ticker)
+        increase_status_Close, percentage_change_Close = get_percentage_change_Close(ticker)
+
+    except Exception as e:
+        print(repr(e))
+        return render_template('errorpage.html')
+        if request.method == 'POST':
+            ticker = request.form['ticker']
+            index()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        ticker = request.form['ticker'].strip()
+    else:
+        ticker = 'AMZN'
+    if not ticker:
+        return render_template('errorpage.html')
+    return redirect(f"/search?ticker={ticker}")
+
+
+@app.route('/search')
+def search_post():
+    ticker = request.args.get("ticker").strip()
+    if not ticker:
+        return render_template('errorpage.html')
+    return return_index_processed(ticker)
+
+
+def update_stock(symbol):
+    while (True):
+        stock = yf.Ticker(symbol)
+        return stock
+
+
+# Function to get today's high value of a stock
+def get_today_high(symbol):
+    global pass_tick
+    pass_tick = symbol
+    stock = update_stock(symbol)
+    data = stock.history(period='1d')
+    if not data.empty:
+        return data['High'].iloc[-1]
+    return None
+
+
+# Function to get today's close value of a stock
+def get_today_close(symbol):
+    while (True):
+        stock = update_stock(symbol)
+        data = stock.history(period='1d')
+        if not data.empty:
+            close = data['Close'].iloc[-1]
+            return round(close, 2)
+        return None
+
+
+# Function to get today's open value of a stock
+def get_today_open(symbol):
+    stock = update_stock(symbol)
+    data = stock.history(period='1d')
+    if not data.empty:
+        return data['Open'].iloc[-1]
+    return None
+
+
+def get_percentage_change_high(symbol):
+    stock = update_stock(symbol)
+    data = stock.history(period='2d')
+    if len(data) >= 2:
+        yesterday_high = data['High'].iloc[-2]
+        today_high = data['High'].iloc[-1]
+        percentage_change = ((today_high - yesterday_high) / yesterday_high) * 100
+        if percentage_change > 0:
+            increase_status = 'Increased'
+        elif percentage_change < 0:
+            increase_status = 'Decreased'
+        else:
+            increase_status = 'No change'
+        return increase_status, round(percentage_change, 3)
+    return None, None
+
+
+def get_percentage_change_Close(symbol):
+    while (True):
+        stock = update_stock(symbol)
+        data = stock.history(period='2d')
+        if len(data) >= 2:
+            yesterday_high = data['Close'].iloc[-2]
+            today_high = data['Close'].iloc[-1]
+            percentage_change = ((today_high - yesterday_high) / yesterday_high) * 100
+            if percentage_change > 0:
+                increase_status = 'Increased'
+            elif percentage_change < 0:
+                increase_status = 'Decreased'
+            else:
+                increase_status = 'No change'
+            return increase_status, (str(round(percentage_change, 3)) + "%")
+        return None, None
+
+
+def get_percentage_change_Open(symbol):
+    stock = update_stock(symbol)
+    data = stock.history(period='2d')
+    if len(data) >= 2:
+        yesterday_high = data['Open'].iloc[-2]
+        today_high = data['Open'].iloc[-1]
+        percentage_change = ((today_high - yesterday_high) / yesterday_high) * 100
+        if percentage_change > 0:
+            increase_status = 'Increased'
+        elif percentage_change < 0:
+            increase_status = 'Decreased'
+        else:
+            increase_status = 'No change'
+        return increase_status, round(percentage_change, 3)
+    return None, None
+
+
+def update_data():
+    while True:
+        sleep(1)
+        if turbo.clients:
+            turbo.push(turbo.update(get_today_close(pass_tick), target="close"))
+            turbo.push(turbo.update(get_percentage_change_Close(pass_tick)[1], target="close_percent"))
+            turbo.push(turbo.update(get_percentage_change_Close(pass_tick)[0], target="close_status"))
+
+
+@app.route('/faq')
+def faq():
+    return render_template('pages-faq.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('pages-contact.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('pages-about.html')
+
+
+@app.route('/overview')
+def overview():
+    return render_template('pages-overview.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('pages-register.html')
+
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+
+@app.route('/gchat')
+def gchat():
+    return render_template('gchat.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('pages-login.html')
+
+
+#with app.app_context():
+threading.Thread(target=update_data).start()
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, use_reloader=False)
